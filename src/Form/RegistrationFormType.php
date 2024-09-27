cee9f3f6396dbf0234e9ac67ddab9be0cf2a79cf
Yoan <yoanjambon21@gmail.com>
2024-09-27 23:21:39 +0200

    Renforcement sécurité inscription

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -5,42 +5,49 @@ namespace App\Form;
 use App\Entity\Users;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Email;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Regex;
 
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('pseudo', TextType::class, [ // Ajoutez le champ pseudo
+            ->add('email', EmailType::class, [
                 'constraints' => [
-                    new NotBlank([
-                        'message' => 'Please enter a pseudo',
-                    ]),
+                    new NotBlank(['message' => 'Please enter your email']),
+                    new Email(['message' => 'The email "{{ value }}" is not a valid email.']),
+                ],
+            ])
+            ->add('pseudo', TextType::class, [
+                'constraints' => [
+                    new NotBlank(['message' => 'Please enter a pseudo']),
                     new Length([
                         'min' => 3,
-                        'minMessage' => 'Your pseudo should be at least {{ limit }} characters',
                         'max' => 50,
-                        'maxMessage' => 'Your pseudo cannot be longer than {{ limit }} characters',
+                    ]),
+                    new Regex([
+                        'pattern' => '/^[a-zA-Z0-9_]+$/',
+                        'message' => 'Pseudo must contain only letters, numbers, and underscores.',
                     ]),
                 ],
             ])
             ->add('plainPassword', PasswordType::class, [
                 'mapped' => false,
                 'constraints' => [
-                    new NotBlank([
-                        'message' => 'Please enter a password',
-                    ]),
-                    new Length([
-                        'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                    new NotBlank(['message' => 'Please enter a password']),
+                    new Length(['min' => 6, 'minMessage' => 'Your password should be at least {{ limit }} characters']),
+                    new Regex([
+                        'pattern' => '/(?=.*[A-Z])(?=.*[0-9])(?=.*\W).{6,}/',
+                        'message' => 'Your password should contain at least one uppercase letter, one number, and one special character.',
                     ]),
                 ],
             ])
