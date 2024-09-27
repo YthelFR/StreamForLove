34286e1091e5f5dbb5722c3fd5ae250513598c32
Yoan <yoanjambon21@gmail.com>
2024-09-27 23:53:02 +0200

    Protection formulaires profile et users

--- src/Form/UsersType.php
+++ src/Form/UsersType.php
@@ -7,19 +7,29 @@ use App\Form\SocialsNetworkType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\Email;
 use Symfony\Component\Validator\Constraints\File;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Regex;
 
 class UsersType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email', null, [
-                'constraints' => new Email(['message' => 'Veuillez entrer une adresse email valide.']),
+            ->add('email', EmailType::class, [
+                'constraints' => [
+                    new NotBlank(['message' => 'L\'email ne peut pas être vide.']),
+                    new Email(['message' => 'Veuillez entrer une adresse email valide.']),
+                ],
             ])
             ->add('roles', ChoiceType::class, [
                 'choices' => [
@@ -30,8 +40,23 @@ class UsersType extends AbstractType
                 'multiple' => true,
                 'expanded' => true,
             ])
-            ->add('password')
-            ->add('pseudo')
+            ->add('password', PasswordType::class, [
+                'constraints' => [
+                    new NotBlank(['message' => 'Le mot de passe ne peut pas être vide.']),
+                    new Length(['min' => 8, 'minMessage' => 'Le mot de passe doit contenir au moins 8 caractères.']),
+                    // Contrôle des caractères spéciaux, majuscules, etc.
+                    new Regex([
+                        'pattern' => '/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/',
+                        'message' => 'Le mot de passe doit contenir au moins une majuscule, une minuscule, un chiffre et un caractère spécial.',
+                    ]),
+                ],
+            ])
+            ->add('pseudo', TextType::class, [
+                'constraints' => [
+                    new NotBlank(['message' => 'Le pseudo ne peut pas être vide.']),
+                    new Length(['min' => 3, 'max' => 20, 'minMessage' => 'Le pseudo doit contenir au moins 3 caractères.']),
+                ],
+            ])
             ->add('avatar', FileType::class, [
                 'label' => 'Avatar (Image file)',
                 'required' => false, // Avatar peut être facultatif
@@ -48,8 +73,9 @@ class UsersType extends AbstractType
                 ],
             ])
             ->add('isValid')
-            ->add('createdAt', null, [
+            ->add('createdAt', DateTimeType::class, [
                 'widget' => 'single_text',
+                'html5' => true,
             ])
             ->add('socialsNetworks', CollectionType::class, [
                 'entry_type' => SocialsNetworkType::class,
@@ -69,6 +95,9 @@ class UsersType extends AbstractType
     {
         $resolver->setDefaults([
             'data_class' => Users::class,
+            'csrf_protection' => true,
+            'csrf_field_name' => '_token',
+            'csrf_token_id' => 'user_form',
         ]);
     }
 }
