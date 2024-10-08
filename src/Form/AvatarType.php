a9c493d47b35824d67a9f97cbdf99a93cef56f5c
Yoan <yoanjambon21@gmail.com>
2024-10-08 19:28:20 +0200

    Tentative correction dahsboard admin

--- src/Form/AvatarType.php
+++ src/Form/AvatarType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\File;
@@ -40,6 +41,9 @@ class AvatarType extends AbstractType
                 'required' => false,
                 'label' => 'Pronoms',
                 'placeholder' => 'Sélectionnez vos pronoms',
+            ])
+            ->add('lien', TextType::class, [
+                'label' => 'Lien de la cagnotte',
             ]);
     }
 
