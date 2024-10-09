c9061cf1dffe549b97b4b50960d7cfc3825cd312
Yoan <yoanjambon21@gmail.com>
2024-10-10 00:19:05 +0200

    Correctin erreur NULL presentations

--- config/bundles.php
+++ config/bundles.php
@@ -18,5 +18,4 @@ return [
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
     Ehyiah\QuillJsBundle\QuillJsBundle::class => ['all' => true],
-    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
